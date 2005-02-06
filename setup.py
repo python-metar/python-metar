ef3aad578168bff3e506a8ae5fec67d769283994
pollard <>
2005-02-06 01:37:28 +0000

    Updated the package description and bumped the version number to 1.2.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Setup script for the metar package
-# $Id: setup.py,v 1.1 2005/01/26 22:30:09 pollard Exp $
+# $Id: setup.py,v 1.2 2005/02/05 20:37:28 pollard Exp $
 #
 # Usage: python setup.py install
 #
@@ -18,34 +18,41 @@ DESCRIPTION="Metar - a package to parse METAR coded weather reports"
 LONG_DESCRIPTION="""
 Metar is a python package for interpreting METAR and SPECI weather reports.
 
-US conventions for METAR/SPECI reports are described in chapter 12 of
-the Federal Meteorological Handbook No.1. (FMH-1 1995), issued by NOAA. 
-See <http://metar.noaa.gov/>
+METAR is the international standard format for reporting weather observations.
+The standard specification for the METAR and SPECI codes is given
+in the WMO Manual on Codes, vol I.1, Part A (WMO-306 I.i.A).  US
+conventions for METAR/SPECI reports are described in chapter 12 of
+the Federal Meteorological Handbook No.1. (FMH-1 1995), issued by
+NOAA.  See <http://metar.noaa.gov/>
 
-International conventions for the METAR and SPECI codes are specified in 
-the WMO Manual on Codes, vol I.1, Part A (WMO-306 I.i.A).  
-
-This module handles a reports that follow the US conventions, as well
-the more general encodings in the WMO spec.  Other regional conventions
-are not supported at present.
-"""
+This module handles all main-body groups in reports that follow the
+WMO spec or the US conventions, except for trend groups.  The most
+useful remark groups defined in the US spec are parsed, as well,
+such as the cumulative precipitation, min/max temperature, peak
+wind and sea-level pressure groups.  No other regional conventions
+are formally supported, but a large number of variant formats found
+in international reports are accepted."""
 
 setup(
     name="metar",
-    version="1.1.0",
+    version="1.2.0",
     author="Tom Pollard",
-    author_email="pollard@alumni.mit.edu",
-    url="",
+    author_email="pollard@alum.mit.edu",
+    url="http://homepage.mac.com/wtpollard/Software/FileSharing4.html",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     download_url="http://homepage.mac.com/wtpollard/Software/FileSharing4.html",
-    license="BSD-style",
+    license="MIT",
     packages=["metar"],
     platforms="Python 2.3 and later.",
     classifiers=[
-    "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering"
+#        "Topic :: Formats and Protocols :: Data Formats",
+#        "Topic :: Scientific/Engineering :: Earth Sciences",
+#        "Topic :: Software Development :: Libraries :: Python Modules"
         ]
     )
