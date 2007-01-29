1c93202e9a13ff63d6d72a8b849f4ad024548acf
pollard <>
2007-01-29 10:41:51 +0000

    Fixed obsolete usage of .str method (should have been .string()).

--- parse_metar.py
+++ parse_metar.py
@@ -53,7 +53,7 @@ def process_line(line):
       obs = Metar.Metar(line)
       if report:
         print "--------------------"
-        print obs.str()
+        print obs.string()
     except Metar.ParserError, err:
       if not silent:
         print "--------------------"
