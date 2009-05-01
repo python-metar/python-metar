cf42f9a538c3f3b10a2e9e12284cab15316afff5
Tom Pollard <tomp@earthlink.net>
2009-05-01 15:55:52 -0400

    Manually merged Toby White's changes to parse the runway state group.

--- metar/Metar.py
+++ metar/Metar.py
@@ -122,6 +122,13 @@ WINDSHEAR_RE = re.compile(r"^(WS\s+)?(ALL\s+RWY|RWY(?P<name>\d\d(RR?|L?|C)?))\s+
 COLOR_RE = re.compile(r"""^(BLACK)?(BLU|GRN|WHT|RED)\+?
                         (/?(BLACK)?(BLU|GRN|WHT|RED)\+?)*\s*""",
                         re.VERBOSE)
+RUNWAYSTATE_RE = re.compile(r"""((?P<name>\d\d) |
+                                 R(?P<namenew>\d\d)(RR?|LL?|C)?/?)
+                                (?P<deposit>(\d|/))
+                                (?P<extent>(\d|/))
+                                (?P<depth>(\d\d|//))
+                                (?P<friction>(\d\d|//))\s+""",
+                             re.VERBOSE)
 TREND_RE = re.compile(r"^(?P<trend>TEMPO|BECMG|FCST|NOSIG)\s+")
 
 TRENDTIME_RE = re.compile(r"(?P<when>(FM|TL|AT))(?P<hour>\d\d)(?P<min>\d\d)\s+")
@@ -700,6 +707,14 @@ class Metar(object):
           trend    [list of strings]
       """
       pass
+ 
+  def _handleRunwayState( self, d ):
+      """
+      Parse (and ignore) the runway state.
+
+      The following attributes are set:
+      """
+      pass
 
   def _handleTrend( self, d ):
       """
@@ -895,6 +910,7 @@ class Metar(object):
                (RECENT_RE,_handleRecent, True), 
                (WINDSHEAR_RE, _handleWindShear, True), 
                (COLOR_RE, _handleColor, True), 
+               (RUNWAYSTATE_RE, _handleRunwayState, True),
                (TREND_RE, _handleTrend, False),
                (REMARK_RE, _startRemarks, False) ]
 
