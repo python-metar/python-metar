64c2a6ff1ed21bbc826c68753df90c5b44c00c0a
Tom Pollard <tpollard@flashpoint-intel.com>
2016-11-19 08:14:19 -0500

    Made sample scripts python3-safe

--- get_report.py
+++ get_report.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 #
+from __future__ import print_function
+
 import os
 import sys
 import getopt
 import string
-import urllib
+try:
+    from urllib2 import urlopen
+except:
+    from urllib.request import urlopen
 from metar import Metar
 
 BASE_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations"
 
 def usage():
   program = os.path.basename(sys.argv[0])
-  print "Usage: ",program,"<station> [ <station> ... ]"
-  print """Options:
+  print("Usage: ",program,"<station> [ <station> ... ]")
+  print("""Options:
   <station> . a four-letter ICAO station code (e.g., "KEWR")
-"""
+""")
   sys.exit(1)
 
 stations = []
@@ -36,19 +41,23 @@ for name in stations:
   if debug: 
     sys.stderr.write("[ "+url+" ]")
   try:
-    urlh = urllib.urlopen(url)
+    urlh = urlopen(url)
     report = ''
     for line in urlh:
+      if not isinstance(line, str):
+        line = line.decode() # convert Python3 bytes buffer to string
       if line.startswith(name):
         report = line.strip()
         obs = Metar.Metar(line)
-        print obs.string()
+        print(obs.string())
         break
     if not report:
-      print "No data for ",name,"\n\n"
-  except Metar.ParserError, err:
-    print "METAR code: ",line
-    print string.join(err.args,", "),"\n"
+      print("No data for ",name,"\n\n")
+  except Metar.ParserError as exc:
+    print("METAR code: ",line)
+    print(string.join(exc.args,", "),"\n")
   except:
-    print "Error retrieving",name,"data","\n"
+    import traceback
+    print(traceback.format_exc())
+    print("Error retrieving",name,"data","\n")
  
--- parse_metar.py
+++ parse_metar.py
@@ -2,6 +2,8 @@
 #
 # simple command-line driver for metar parser
 #
+from __future__ import print_function
+
 import sys, os
 from metar import Metar
 import string
@@ -10,8 +12,8 @@ import profile, pstats
 
 def usage():
   program = os.path.basename(sys.argv[0])
-  print "Usage: ",program,"[-s] [<file>]"
-  print """Options:
+  print("Usage: ",program,"[-s] [<file>]")
+  print("""Options:
   <file> ... a file containing METAR reports to parse
   -q ....... run "quietly" - just report parsing error.
   -s ....... run silently. (no output)
@@ -20,7 +22,7 @@ This program reads lines containing coded METAR reports from a file
 and prints human-reable reports.  Lines are taken from stdin if no
 file is given.  For testing purposes, the script can run silently,
 reporting only when a METAR report can't be fully parsed.
-"""
+""")
   sys.exit(1)
 
 files = []
@@ -48,17 +50,17 @@ except:
 def process_line(line):
   """Decode a single input line."""
   line = line.strip()
-  if len(line) and line[0] in string.uppercase:
+  if len(line) and line[0] == line[0].upper():
     try:
       obs = Metar.Metar(line)
       if report:
-        print "--------------------"
-        print obs.string()
-    except Metar.ParserError, err:
+        print("--------------------")
+        print(obs.string())
+    except Metar.ParserError as exc:
       if not silent:
-        print "--------------------"
-        print "METAR code: ",line
-        print string.join(err.args,", ")
+        print("--------------------")
+        print("METAR code: ",line)
+        print(string.join(exc.args,", "))
 
 def process_files(files):
   """Decode METAR lines from the given files."""
@@ -85,4 +87,4 @@ else:
 
 if prof:
   ps = pstats.load('metar.prof')
-  print ps.strip_dirs().sort_stats('time').print_stats()
+  print(ps.strip_dirs().sort_stats('time').print_stats())
