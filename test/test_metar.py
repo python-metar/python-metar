dfb4985190932bfe8d1e810be10d63ab00290a36
ro <mail2benr@gmail.com>
2020-10-17 12:14:00 +0200

    Add test for RVR with slashes

--- test/test_metar.py
+++ test/test_metar.py
@@ -125,6 +125,15 @@ def test_issue107_runwayunits():
     res = report.runway_visual_range("FT")
     assert res == "on runway 28L, 4921 feet"
 
+@pytest.mark.parametrize("RVR", ["R28L/////", "R28L/////FT", "R28L//////", "R28L/////N"])
+def test_issue26_runway_slashes(RVR):
+    """Check RVR with slashes decoding to missing value set as -999."""
+    report = Metar.Metar(
+        "METAR KPIT 091955Z COR 22015G25KT 3/4SM {} TSRA OVC010CB "
+        "18/16 A2992 RMK SLP045 T1820160".format(RVR)
+    )
+    assert pytest.approx(report.runway[0][1].value()) == -999.0
+
 def test_010_parseType_default():
     """Check default value of the report type."""
     assert Metar.Metar("KEWR").type == "METAR"
